08a51a77575600637421423d94a6a871ee2502c1
Aditya kumar choudhary <aditya65489@gmail.com>
2026-08-26 01:25:49 +0530

    Uploading my python notes

---
+++ python_string.py
@@ -0,0 +1,55 @@
+#%%
+#stg= "tim's birthday"
+#print (stg)
+#stg1= 'tim said, "I\'m busy today" '
+#print (stg1)
+#stg2= '''hey there!
+#welcome to python practice'''
+#print(stg2)
+#print(len(stg2))
+#print(stg2[8])
+#for i in stg2:
+    #print(i)
+    #print(i, end="")
+#stg3= "welcome to my world"
+#print(stg3[0:6])
+#print(stg3[:6])
+#print(stg3[6:])
+#print(stg3[2:5])
+#print(stg3.upper())
+#print(stg3.lower())
+#print(stg3.find('w'))
+#print(stg3.index('l'))
+#print(stg3.replace('w','x'))
+#print(stg3.split(" "))
+#x = stg3.split(" ")
+#print(x)
+
+#print(stg3.rpartition("to"))
+#stg1="good"
+#stg2="morning"
+#print(stg1+" "+stg2)
+
+#stg1="hey"
+#stg2="there"
+#stg3="all"
+#stg="{}, {}, {}!".format(stg1,stg2,stg3)
+#print(stg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+#%%
