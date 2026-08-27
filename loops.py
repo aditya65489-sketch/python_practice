e1e5ff8f58e893f001ec79e0cdac3c30ea037b56
Aditya kumar choudhary <aditya65489@gmail.com>
2026-08-27 16:45:35 +0530

    Uploading my python notes

---
+++ loops.py
@@ -0,0 +1,29 @@
+#var= int(input("Enter a multiple of 7"))
+#while var%7 !=0:
+    #var= int(input("Enter a multiple of 7"))
+#else:
+    #print("%d is a multiple of 7" % var)
+
+#x = [1,6,"simpliearn"]
+#for i in x:
+    #print(i)
+
+#x = "simpliearn"
+#for i in x:
+    #print(i)
+
+#x= [1,2,3,],["a","b","c"]
+#for i in x:
+    #for j in i:
+        #print(j,end=" ")
+
+#x= "hey there. how are you?"
+#for i in x:
+    #if i == ".":
+        #break
+    #print(i, end=" ")
+
+"""for i in [1,13,56,4,6]:
+    if i>14:
+        continue
+    print(i)"""
