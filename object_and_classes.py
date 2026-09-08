ea0b27fbf8c6e2e2f21acf89c787847086cb61c6
Aditya kumar choudhary <aditya65489@gmail.com>
2026-09-09 02:23:57 +0530

    Uploading my python notes

--- object_and_classes.py
+++ object_and_classes.py
@@ -18,7 +18,7 @@ obj= Person()
 Person.talk(obj)
 Person.vote(obj)'''
 
-class Person :
+'''class Person :
     def __init__(self,n,g,a) :
         self.name = n
         self.gender = g
@@ -40,4 +40,71 @@ obj1.talk()
 obj1.vote()
 
 obj2.talk()
-obj2.vote()
+obj2.vote()'''
+
+
+'''class car :
+    def __init__(self, year, speed) :
+        self.year = year
+        self.speed = speed
+    def getSpeed(self) :
+        print("maximum speed is:", self.speed)
+    def setSpeed(self, speed) :
+        self.speed = speed
+
+BMW = car(2018, 155)
+FORD = car(2016, 140)
+
+BMW.getSpeed()
+BMW.setSpeed(143)
+
+BMW.getSpeed()
+FORD.getSpeed()'''
+
+
+'''class car :
+    def __init__(self, year, speed) :
+        self.year = year
+        self.speed = speed
+    def getSpeed(self) :
+        print("maximum speed is:", self.speed)
+    def setSpeed(self, speed) :
+        self.speed = speed
+        #print(self.speed)
+
+BMW = car(2018, 155)
+FORD = car(2016, 140)
+#inheritence
+class Sedan(car) :   #child class
+    def accelerate(self) :
+        print('137')
+    def openTrunk(self) :
+        print("trunk has been opened")
+
+class SUV(car): #child class
+    def accelerate(self) :
+        print('127')
+Honda = Sedan(2018, 150)
+BMW.getSpeed()
+Honda.getSpeed()
+Honda.openTrunk()
+Honda.accelerate()'''
+
+#polymorphism
+class car :     #parent class
+    def __init__(self, name) :
+        self.name = name
+
+class Sedan(car) :   #child class
+    def accelerate(self) :
+        print('137')
+
+class SUV(car): #child class
+    def accelerate(self) :
+        print('127')
+
+objL = [Sedan("camry"), SUV("scorpio")]
+
+for obj in objL :
+    print (obj.name+" : ", end="")
+    obj.accelerate()
