772fdd219fe81aeab51c4f26c7ef694e18abb175
Aditya kumar choudhary <aditya65489@gmail.com>
2026-09-12 02:20:46 +0530

    Uploading my python notes

--- thread.py
+++ thread.py
@@ -1,6 +1,6 @@
-'''from threading import *
+from threading import *
 def show () :
     print("this is a child thread")
 t=  Thread(target=show())
 t.start()
-print("this is parent thread")'''
+print("this is parent thread")
